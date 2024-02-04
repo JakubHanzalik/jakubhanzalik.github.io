96cf32354ad2cd210d9b694de69fd9f112ac0ae6
Jakub Hanzalik <jakub.hanzalik@gmail.com>
2024-02-04 15:37:52 +0100

    Init

---
+++ assets/DetectPWA-yJ6SmiTg.js
@@ -0,0 +1,2 @@
+function r(){return window.matchMedia("(display-mode: standalone)").matches||window.navigator.standalone?!0:("serviceWorker"in navigator&&navigator.serviceWorker.getRegistrations().then(e=>{if(e.length>0)return!0}),!1)}export{r as i};
+//# sourceMappingURL=DetectPWA-yJ6SmiTg.js.map
